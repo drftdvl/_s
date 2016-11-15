fcb003385aad47a940e1d7a0d96bd1e45b5ef782
charlessamoy <charlessamoy@Charless-MacBook-Pro.local>
2016-11-15 18:54:51 +0800

    Added some things to home

--- home.php
+++ home.php
@@ -1,9 +1,54 @@
 <?php
 	get_header();
+	/* Template Name: Feature */
 ?>
+<!DOCTYPE HTML>
+<html>
 
-<h1>Home</h1>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
 
+<style>
+@font-face
+{
+	font-family: PlayFairBlackItalic;
+	src: url('/box/wp-content/themes/_s/fonts/PlayFairDisplay-BlackItalic.otf');
+}
+
+@font-face
+{
+	font-family: QuickSandBold;
+	src: url('/box/wp-content/themes/_s/fonts/Quicksand-Bold.otf');
+}
+
+@font-face
+{
+	font-family:QuickSandRegular;
+	src: url('/box/wp-content/themes/_s/fonts/Quicksand-regular.otf');
+}
+
+body
+{
+	background-image: url("/box/wp-content/themes/_s/1.jpg")
+}
+
+.bioOrg
+{
+	border-style: hidden;
+	border-top: : 0px;
+	font-family: PlayFairBlackItalic;
+	color: white;
+	font-size:48px;
+}
+</style>
+<body>
+	<center>
+	<div>
+			<img src="/box/wp-content/themes/_s/BOx.Logo.W.png" alt = "Ateneo BOx:eXplore. eXperience. eXcel" style="width:240px;height:130px;margin-top:160px;">
+			<p class = "bioOrg">Ateneo Biological Organization</p>
+
+	</div>
+	</center>
+</body>
 <?php
 get_footer();
 ?>
